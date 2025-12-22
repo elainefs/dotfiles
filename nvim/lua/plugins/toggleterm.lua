3adf5e5d39a7f4732d67744745d888759fd769b7
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-12-22 18:19:31 -0300

    change default toggleterm mode

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,20 +3,22 @@ return {
   version = "*",
   config = function()
     require("toggleterm").setup({
-      size = 10,
+      size = 15,
       open_mapping = [[<C-j>]],
       shade_terminals = true,
       shading_factor = 2,
-      direction = "float",
+      direction = "horizontal",
       float_opts = {
         border = "curved",
       },
     })
 
     -- To use terminal side-by-side in horizontal direction: 2<leader>th
-    vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
+    -- vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
     -- To use terminal side-by-side in vertical direction: 2<leader>tv
     vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
+    -- To use terminal in floating mode
+    vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true })
 
   end,
 }
