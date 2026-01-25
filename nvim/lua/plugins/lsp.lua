476b6425191addfb4b6a65c6f53efa4e17abdebb
Elaine Ferreira <elaineferreira.dev@gmail.com>
2026-01-25 17:56:26 -0300

    refactor lsp.lua for new version of neovim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,42 +21,51 @@ return {
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
     },
+
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          if server_name == "pylsp" then
-            vim.lsp.config.pylsp = {
-              capabilities = capabilities,
-              settings = {
-                pylsp = {
-                  plugins = {
-                    pycodestyle = { enabled = false },
-                    pyflakes = { enabled = false },
-                    pylint = { enabled = false },
-                    black = { enabled = false },
-                    autopep8 = { enabled = false },
-                    yapf = { enabled = false },
-                    mccabe = { enabled = false },
-                    pylsp_mypy = { enabled = false },
-                    pylsp_isort = { enabled = false },
-                    pylsp_black = { enabled = false },
-                  },
+      local mason_lsp = require("mason-lspconfig")
+
+      mason_lsp.setup()
+
+      -- Get installed servers
+      local servers = mason_lsp.get_installed_servers()
+
+      for _, server in ipairs(servers) do
+        if server == "pylsp" then
+          vim.lsp.config("pylsp", {
+            capabilities = capabilities,
+
+            settings = {
+              pylsp = {
+                plugins = {
+                  pycodestyle = { enabled = false },
+                  pyflakes = { enabled = false },
+                  pylint = { enabled = false },
+                  black = { enabled = false },
+                  autopep8 = { enabled = false },
+                  yapf = { enabled = false },
+                  mccabe = { enabled = false },
+                  pylsp_mypy = { enabled = false },
+                  pylsp_isort = { enabled = false },
+                  pylsp_black = { enabled = false },
                 },
               },
-            }
-            vim.lsp.enable("pylsp")
-          else
-            -- Automatically configure each installed server
-            vim.lsp.config[server_name] = {
-              capabilities = capabilities,
-            }
-            vim.lsp.enable(server_name)
-          end
-        end,
-      })
+            },
+          })
 
+          vim.lsp.enable("pylsp")
+        else
+          vim.lsp.config(server, {
+            capabilities = capabilities,
+          })
+
+          vim.lsp.enable(server)
+        end
+      end
+
+      -- Keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Documentation" })
       vim.keymap.set("n", "<F2>", vim.lsp.buf.definition, { desc = "Go to Definition" })
       vim.keymap.set("n", "<F3>", vim.lsp.buf.references, { desc = "Show References" })
