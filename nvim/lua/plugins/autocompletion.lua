635faa9b5a7e624387758e5c71a84b987de19827
Elaine Ferreira <elaineferreira.dev@gmail.com>
2026-01-25 17:55:32 -0300

    improve autocompletion.lua

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -83,40 +83,14 @@ return {
 			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			mapping = cmp.mapping.preset.insert({
-				-- Select the next item
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				-- Select the previous item
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-				-- Scroll the documentation window [b]ack / [f]orward
-				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<Tab>"] = cmp.mapping.select_next_item(), -- Select the next item
+				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Select the previous item
 
 				-- Accept the completion
 				["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-				-- ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
 
 				-- Manually trigger a completion from nvim-cmp.
 				["<C-Space>"] = cmp.mapping.complete({}),
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
 			}),
 			sources = {
 				{
