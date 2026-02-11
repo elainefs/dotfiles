3e64f371d125360afa788c35cf646e9eb438b30d
Elaine Ferreira <elaineferreira.dev@gmail.com>
2026-02-11 00:10:03 -0300

    remove mypy from nonels

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -19,7 +19,6 @@ return {
 				"ruff", -- Python linter and formatter
 				"phpcbf", -- Formatter for PHP
 				"phpcs", -- Linter for PHP
-        "mypy"  -- Type Hints for Python
 			},
 			automatic_installation = true,
 		})
@@ -33,7 +32,6 @@ return {
 			formatting.djlint.with({ extra_args = { "--profile=django" } }),
 			formatting.phpcbf.with({ extra_args = { "--standard=PSR12" } }),
 			diagnostics.phpcs.with({ extra_args = { "--standard=PSR12" } }),
-      diagnostics.mypy,
 			require("none-ls.formatting.ruff").with({
 				extra_args = { "--extend-select", "I" },
 			}),
