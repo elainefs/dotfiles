4f01b5eb99e83a6eec993348c214c76e07c73c83
Elaine Ferreira <elaineferreira.dev@gmail.com>
2026-04-19 11:45:40 -0300

    set conceallevel to 0

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -44,7 +44,7 @@ vim.api.nvim_create_autocmd("CursorHold", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.opt_local.conceallevel = 2
+    vim.opt_local.conceallevel = 0
   end,
 })
 
