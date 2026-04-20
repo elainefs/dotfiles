851cd6cfb3165115d7377288e5f7cf44493a308c
Elaine Ferreira <elaineferreira.dev@gmail.com>
2026-04-20 01:39:09 -0300

    improve script for navigating through windows

--- .config/sway/scripts/window-switcher.sh
+++ .config/sway/scripts/window-switcher.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-selected=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[]? | .. | select(.type? == "con" and .name? != null) | "\(.name) — [ID: \(.id)]"' | rofi -dmenu -i -m 1 -p "Windows")
+window_list=$(swaymsg -t get_tree | jq -r '
+    .. 
+    | select(.type? == "con" or .type? == "floating_con") 
+    | select(.name? != null and .name? != "__i3_scratch") 
+    | "\(.name) \tID:\(.id)"')
 
-if [ -n "$selected" ]; then
-    win_id=$(echo "$selected" | grep -oP '\[ID: \K[0-9]+(?=\])')
+selected_name=$(echo "$window_list" | cut -f1 | rofi -dmenu -i -m 1 -p "Windows")
+
+if [ -n "$selected_name" ]; then
+    win_id=$(echo "$window_list" | grep "^$selected_name" | awk -F'ID:' '{print $2}' | head -n 1)
 
     swaymsg "[con_id=$win_id] focus"
 fi
